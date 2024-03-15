a3ceec03726a37d3222686fa4e892345207f4c9f
BradleySeguraBorbon <bradley.segura.borbon@est.una.ac.cr>
2024-03-15 08:53:41 -0600

    Project Setup
    Controller, model, service, util, resources and view packages were created. External support libraries were included into the util package

---
+++ src/main/java/cr/ac/una/tarea_bradleysegura_noemimurillo_programacion_ii/App.java
@@ -0,0 +1,30 @@
+package cr.ac.una.tarea_bradleysegura_noemimurillo_programacion_ii;
+
+import cr.ac.una.tarea_bradleysegura_noemimurillo_programacion_ii.util.FlowController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FlowController.getInstance().InitializeFlow(stage, null);
+        stage.setTitle("SISTEMA DE COOPERATIVA FINANCIERA");
+        FlowController.getInstance().goViewInWindow("Test");
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
